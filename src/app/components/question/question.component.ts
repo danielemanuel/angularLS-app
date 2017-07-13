4dc0bdc6757fd0514b0509090937e35bba3814f2
danielemanuel <daniel.costea@vkernel.ro>
2017-07-13 15:04:44 +0100

    * Persisting data to local storage * added question are saved in local storage * you can delete questions from the local storage

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { DataService } from "../../services/data.service";
 import { Question } from '../../models/Question';
 
 @Component({
@@ -10,9 +10,14 @@ import { Question } from '../../models/Question';
 export class QuestionComponent implements OnInit {
   @Input('question') question:Question;
 
-  constructor() { }
+  constructor(
+    public dataService: DataService
+  ) { }
 
   ngOnInit() {
   }
 
+removeQuestion(question:Question){
+  this.dataService.removeQuestion(question);
+}
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,31 +8,67 @@ export class DataService {
 
   constructor() {
 
-    this.questions = [
-      {
-        text:'What is your name?',
-        answer: "My name is Daniel",
-        hide: true
-      },
-      {
-        text:'What is your favourite color?',
-        answer: "My favorite color is red",
-        hide: true
-      },
-      {
-        text:'What is your favourite language?',
-        answer: "My favourite language is javascript",
-        hide: true
-      }
-    ];
+    // this.questions = [
+    //   {
+    //     text:'What is your name?',
+    //     answer: "My name is Daniel",
+    //     hide: true
+    //   },
+    //   {
+    //     text:'What is your favourite color?',
+    //     answer: "My favorite color is red",
+    //     hide: true
+    //   },
+    //   {
+    //     text:'What is your favourite language?',
+    //     answer: "My favourite language is javascript",
+    //     hide: true
+    //   }
+    // ];
   }
 
+  //Get question from LS
   getQuestions(){
+    if(localStorage.getItem('questions') === null){
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
   }
 
+  //Add questions from LS
   addQuestion(question:Question){
-      this.questions.unshift(question);
+    this.questions.unshift(question);
+
+    //Init local var
+    let questions;
+
+    if(localStorage.getItem('questions') === null){
+      questions = [];
+      //Push new question
+      questions.unshift(question);
+      //Set new array to LS
+      localStorage.setItem('questions', JSON.stringify(questions));
+    } else {
+      questions = JSON.parse(localStorage.getItem("questions"));
+
+      //Add new question
+      questions.unshift(question);
+
+      //Reset LS
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
+
   }
 
+  //Remove questions from LS
+  removeQuestion(question:Question){
+    for(let i=0; i < this.questions.length; i++){
+      if(question == this.questions[i]){
+        this.questions.splice(i, 1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
+      }
+    }
+  }
 }
